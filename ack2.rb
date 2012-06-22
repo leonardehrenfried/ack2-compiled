aeba439a2131da21a915a60b92c2776002c0fe4c
Leonard Ehrenfried <leonard.ehrenfried@commercetools.de>
2012-06-22 15:59:15 +0200

    Add homebrew formula

---
+++ ack2.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Ack2 < Formula
+  homepage 'http://betterthangrep.com/'
+  url 'https://raw.github.com/lenniboy/ack2-compiled/master/ack-standalone-2_00a01'
+  sha1 '2a2e9cf1517184272dbc06bfeca7d64c5d376ab5'
+  version "2.00a01"
+
+  def install
+    ver = @version.sub(".","_")
+    binary = "ack-standalone-#{ver}"
+    FileUtils.cp binary, "ack2"
+    bin.install 'ack2'
+  end
+
+  def test
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test ack-standalone-2_00a`.
+    system "false"
+  end
+end
