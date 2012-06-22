fb9fa33a061279ebbcb5a3486de69bd2138ab9e8
Leonard Ehrenfried <leonard.ehrenfried@commercetools.de>
2012-06-22 15:01:39 +0200

    Add build script and first built file

---
+++ build.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash -e
+
+# you need to have File:Next and Test:Next installed for this to work
+
+BASE=`pwd`
+cd ../ack2
+echo "Change working dir to ../ack2"
+git pull
+perl Makefile.PL
+make
+make test
+VERSION=`cat Makefile|grep "VERSION_SYM = "|cut -d" " -f 3`
+cp ack-standalone ../ack2-compiled/ack-standalone-$VERSION
+
